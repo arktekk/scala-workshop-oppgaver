3c66454880f37f577b38fe6c625bf114fd536b16
Kristian Nordal <kristian.nordal@arktekk.no>
2011-05-04 18:10:47 +0200

    o La til addToPackages og noticesAutoFadeOut i Boot

--- oppgaver/lift-intro/src/main/scala/bootstrap/liftweb/Boot.scala
+++ oppgaver/lift-intro/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,7 +1,10 @@
 package bootstrap.liftweb
 
 import net.liftweb.sitemap.{SiteMap, Menu}
-import net.liftweb.http.LiftRules
+import net.liftweb.http.{NoticeType, LiftRules}
+import net.liftweb.common.Full
+import net.liftweb.util.Helpers._
+
 class Boot {
 
   def boot {
@@ -10,6 +13,10 @@ class Boot {
       Menu("index") / "index"
     )
 
+    LiftRules.addToPackages("meldingsserver")
+
     LiftRules.setSiteMapFunc(() => SiteMap(sitemap: _*))
+
+    LiftRules.noticesAutoFadeOut.default.set((noticeType: NoticeType.Value) => Full((2 seconds, 4 seconds)))
   }
 }
